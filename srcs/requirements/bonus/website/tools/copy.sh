881f512d7f476b89787590d21c138f5ff9c6d4bd
vgoret <victor.goret@laposte.net>
2024-05-29 15:52:52 +0200

    css working

--- srcs/requirements/bonus/website/tools/copy.sh
+++ srcs/requirements/bonus/website/tools/copy.sh
@@ -1,13 +1,3 @@
 #!bin/bash
-
-# if [ ! -f "/var/www/html/index.html" ]; then
-    # mv /data/website/* /var/www/html
-# fi
-
-# cd /data/website/src
-
-# mkdir -p /static_website
-# cp /data/website/src/* /static_website
-# cp /static_website/src/index.html /static_website/index.html
 cd /
 python3 -m http.server
