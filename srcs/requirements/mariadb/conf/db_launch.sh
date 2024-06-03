ad1cbd38233ccf06956a14f829ab85899d231b60
vgoret <victor.goret@laposte.net>
2024-06-03 18:07:37 +0200

    comments

--- srcs/requirements/mariadb/conf/db_launch.sh
+++ srcs/requirements/mariadb/conf/db_launch.sh
@@ -4,20 +4,22 @@ echo "\n\033[2;31m==============================\033[0m"
 echo "\n\033[2;31m=== Database configuration ===\033[0m"
 echo "\n\033[2;31m==============================\033[0m\n"
 
+#VERIFIE SI LE DOSSIER DE LA DB EXISTE DEJA
 if [ -d "/var/lib/mysql/${SQL_DATABASE}" ]
 then 
     echo "==> database ${SQL_DATABASE} already exists\n"
 else
     echo "starting mariadb..."
-    service mariadb start
+    service mariadb start #demare le service
     sleep 1
     echo "creating database: ${SQL_DATABASE}"
-    mysql -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
-    mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-    mysql -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-    mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+    mysql -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};" #CREER LA BASE DE DONNEE SI ELLE N'EXISTE PAS
+    mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';" #CREER UN USER AVEC UN PASS SI IL N'EXISE PAS DEJA
+    mysql -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"  #DONNE TOUTES LES PERMS SUR LA DB A SQL_USER
+    mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';" #DONNE TOUTES LES PERMS A ROOT POUR LES CONNEXIONS DEPUIS LOCALHOST AVEC LE ROOT PASS
     sleep 1
     # shutting down mariadb so it can be restarted using exec
+    # needed because like this mysqlq becomes processus #1 in container
     mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
     echo "database ready"
 fi
@@ -25,4 +27,5 @@ sleep 1
 # using exec, the specified command becomes PID 1
 # runs the command without a shell. It can have advantages in term of signal handling and clean process termination
 echo "Database up and ready !\n" 
+# exec is good because ca assure que docker peut restart mysqld si ya un soucis
 exec mysqld
--- srcs/requirements/wordpress/tools/wp-cli_autoconfig.sh
+++ srcs/requirements/wordpress/tools/wp-cli_autoconfig.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
 sleep 5
-cd /var/www/html
+cd /var/www/html #va dans le bon dossier
+#verifie si le fichier existe ou pas. Si il existe pas besoin de reconfig
 if [ ! -f wp-config.php ];
 then
     sleep 5
+    #Telecharge le noyau de WP en root
     ./wp-cli.phar core download --allow-root
 
+    #Creer le wp-config.php avec les params de la DB
     ./wp-cli.phar config create --allow-root \
                 --dbname=$SQL_DATABASE \
                 --dbuser=$SQL_USER \
                 --dbpass=$SQL_PASSWORD \
                 --dbhost=mariadb:3306\
 
+    #Telecharge WP avec les donnees du .env
     ./wp-cli.phar core install --allow-root \
                 --url=$DOMAIN_NAME \
                 --title="$SITE_TITLE" \
@@ -20,27 +24,34 @@ then
                 --admin_password=$ADMIN_PASSWORD \
                 --admin_email=$ADMIN_EMAIL \
 
+    #Creer un user avec le role autheur
     ./wp-cli.phar user create $USER1_LOGIN $USER1_MAIL --allow-root \
                 --role=author \
                 --user_pass=$USER1_PASS \
 
+    #Affiche la liste des themes
     ./wp-cli.phar theme list --allow-root
 
+    #Defini et set le theme de la template
     THEME_NAME="retrogeek" # Remplacez 'votre-theme' par le nom de votre thème
     ./wp-cli.phar theme install $THEME_NAME --activate --allow-root
     
+    #Creer un poste
     ./wp-cli.phar post create --post_type=post --post_title='105/100 ou bagarre.' --post_content="<!-- wp:paragraph {\"style\":{\"elements\":{\"link\":{\"color\":{\"text\":\"var:preset|color|retrogeek-background\"}}}},\"backgroundColor\":\"retrogeek-link\",\"textColor\":\"retrogeek-background\"} --><p class=\"has-retrogeek-background-color has-retrogeek-link-background-color has-text-color has-background has-link-color\">Jme suis tape des heures de docs incomprehensibles pour ce resultat de fou furieux je merite bien 105/100 nan ?</p><!-- /wp:paragraph --><!-- wp:paragraph {\"style\":{\"color\":{\"text\":\"#fe0000\"},\"elements\":{\"link\":{\"color\":{\"text\":\"#fe0000\"}}}},\"backgroundColor\":\"retrogeek-link\"} --><p class=\"has-retrogeek-link-background-color has-text-color has-background has-link-color\" style=\"color:#fe0000\">Sinon jte DDOS.</p><!-- /wp:paragraph --><!-- wp:paragraph {\"backgroundColor\":\"retrogeek-link\"} --><p class=\"has-retrogeek-link-background-color has-background\">⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡠⢤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀<br>⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠟⠃⠀⠀⠙⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀<br>⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠋⠀⠀⠀⠀⠀⠀⠘⣆⠀⠀⠀⠀⠀⠀⠀⠀<br>⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠾⢛⠒⠀⠀⠀⠀⠀⠀⠀⢸⡆⠀⠀⠀⠀⠀⠀⠀<br>⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣶⣄⡈⠓⢄⠠⡀⠀⠀⠀⣄⣷⠀⠀⠀⠀⠀⠀⠀<br>⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣷⠀⠈⠱⡄⠑⣌⠆⠀⠀⡜⢻⠀⠀⠀⠀⠀⠀⠀<br>⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡿⠳⡆⠐⢿⣆⠈⢿⠀⠀⡇⠘⡆⠀⠀⠀⠀⠀⠀<br>⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣷⡇⠀⠀⠈⢆⠈⠆⢸⠀⠀⢣⠀⠀⠀⠀⠀⠀<br>⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣧⠀⠀⠈⢂⠀⡇⠀⠀⢨⠓⣄⠀⠀⠀⠀<br>⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⣿⣦⣤⠖⡏⡸⠀⣀⡴⠋⠀⠈⠢⡀⠀⠀<br>⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⠁⣹⣿⣿⣿⣷⣾⠽⠖⠊⢹⣀⠄⠀⠀⠀⠈⢣⡀<br>⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡟⣇⣰⢫⢻⢉⠉⠀⣿⡆⠀⠀⡸⡏⠀⠀⠀⠀⠀⠀⢇<br>⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢨⡇⡇⠈⢸⢸⢸⠀⠀⡇⡇⠀⠀⠁⠻⡄⡠⠂⠀⠀⠀⠘<br>⢤⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠛⠓⡇⠀⠸⡆⢸⠀⢠⣿⠀⠀⠀⠀⣰⣿⣵⡆⠀⠀⠀⠀<br>⠈⢻⣷⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡿⣦⣀⡇⠀⢧⡇⠀⠀⢺⡟⠀⠀⠀⢰⠉⣰⠟⠊⣠⠂⠀⡸<br>⠀⠀⢻⣿⣿⣷⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⢧⡙⠺⠿⡇⠀⠘⠇⠀⠀⢸⣧⠀⠀⢠⠃⣾⣌⠉⠩⠭⠍⣉⡇<br>⠀⠀⠀⠻⣿⣿⣿⣿⣿⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣞⣋⠀⠈⠀⡳⣧⠀⠀⠀⠀⠀⢸⡏⠀⠀⡞⢰⠉⠉⠉⠉⠉⠓⢻⠃<br>⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣷⡄⠀⠀⢀⣀⠠⠤⣤⣤⠤⠞⠓⢠⠈⡆⠀⢣⣸⣾⠆⠀⠀⠀⠀⠀⢀⣀⡼⠁⡿⠈⣉⣉⣒⡒⠢⡼⠀<br>⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣎⣽⣶⣤⡶⢋⣤⠃⣠⡦⢀⡼⢦⣾⡤⠚⣟⣁⣀⣀⣀⣀⠀⣀⣈⣀⣠⣾⣅⠀⠑⠂⠤⠌⣩⡇⠀<br>⠀⠀⠀⠀⠀⠀⠘⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡁⣺⢁⣞⣉⡴⠟⡀⠀⠀⠀⠁⠸⡅⠀⠈⢷⠈⠏⠙⠀⢹⡛⠀⢉⠀⠀⠀⣀⣀⣼⡇⠀<br>⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣽⣿⡟⢡⠖⣡⡴⠂⣀⣀⣀⣰⣁⣀⣀⣸⠀⠀⠀⠀⠈⠁⠀⠀⠈⠀⣠⠜⠋⣠⠁⠀<br>⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⡟⢿⣿⣿⣷⡟⢋⣥⣖⣉⠀⠈⢁⡀⠤⠚⠿⣷⡦⢀⣠⣀⠢⣄⣀⡠⠔⠋⠁⠀⣼⠃⠀⠀<br>⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⡄⠈⠻⣿⣿⢿⣛⣩⠤⠒⠉⠁⠀⠀⠀⠀⠀⠉⠒⢤⡀⠉⠁⠀⠀⠀⠀⠀⢀⡿⠀⠀⠀<br>⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⣤⣤⠴⠟⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠑⠤⠀⠀⠀⠀⠀⢩⠇⠀⠀⠀<br>⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀(c'est moi qui hack ton PC)</p><!-- /wp:paragraph --><!-- wp:paragraph {\"style\":{\"elements\":{\"link\":{\"color\":{\"text\":\"var:preset|color|retrogeek-background\"}}}},\"backgroundColor\":\"retrogeek-link\",\"textColor\":\"retrogeek-background\"} --><p class=\"has-retrogeek-background-color has-retrogeek-link-background-color has-text-color has-background has-link-color\">(ascii art en libre service <a href=\"https://emojicombos.com/hacker-ascii-art\" style=\"color:red\">ICI</a> !!)</p><!-- /wp:paragraph -->" --post_status=publish --post_author='le_grand_manitou' --allow-root
 
+    #Change le proprietaire en www-data
     chown -R www-data:www-data /var/www/html/wp-content
     chown -R www-data:www-data /var/www/html
     
     echo "Wordpress is now setup"
 
 else
+    #Nodifie le UID et GID de www-daata pour que ca corresponde au user de NGINX
     usermod -u 33 www-data && groupmod -g 33 www-data
     chown -R www-data:www-data /var/www/html/
     echo "Wordpress is already setup"
 
 fi
 
+#Exec php-fpm pour traiter les scriptss dans le container
 exec /usr/sbin/php-fpm7.4 -F;
